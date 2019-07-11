7be623a6dccc1d7b7a820019d9513aebe4b71f99
David <dmchoull@gmail.com>
2019-07-11 16:12:31 -0400

    changed to the correct URL

--- src/main/kotlin/exercise5/exercise5.kt
+++ src/main/kotlin/exercise5/exercise5.kt
@@ -1,7 +1,7 @@
 package exercise5
 
 // 💯 Bonus Exercise 5: Simple DSL to generate Mars Rovers input files
-// https://kotlinlang.org/docs/reference/functions.html#variable-number-of-arguments-varargs
+// https://kotlinlang.org/docs/reference/type-safe-builders.html
 
 // 🧠 Start by defining a mission function that will support the usage see in the test. You will likely want to create
 // a few other classes to support the usage shown.
